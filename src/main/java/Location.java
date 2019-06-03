df7a8e63c176821273c6eee91970e67eade74e74
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-06-03 19:07:18 +0300

    Add Location class and LocationTest. This commit will also add methods to Location class such as update, delete find and all and all the tests.

---
+++ src/main/java/Location.java
@@ -0,0 +1,75 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Location {
+    private int id;
+    private String name;
+
+    public Location(String name){
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if(!(otherObject instanceof Location))
+            return false;
+        else {
+            Location comparisonObj = (Location) otherObject;
+            return this.id == comparisonObj.getId() && this.name.equals(comparisonObj.getName());
+        }
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO locations (name) VALUES(:name)";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Location> all(){
+
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM locations")
+                    .executeAndFetch(Location.class);
+        }
+    }
+
+    public static Location find(int id){
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM locations WHERE id=:id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Location.class);
+        }
+    }
+
+    public void update(String name, int badgeNumber){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "UPDATE locations SET name = :name WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    public void delete() {
+        try(Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM locations WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+}
---
+++ src/test/java/LocationTest.java
@@ -0,0 +1,95 @@
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+public class LocationTest {
+
+    @Before
+    public void setUp()  {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "verisence", "pass");
+    }
+
+    @After
+    public void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+            String sqlLocation = "DELETE FROM locations *;";
+            con.createQuery(sqlLocation).executeUpdate();
+        }
+    }
+
+    @Test
+    public void Location_instantiatesCorrectly_true(){
+        Location location = new Location("Donson");
+        assertEquals(true, location instanceof Location);
+    }
+    @Test
+    public void Location_instantiatesWithName_String() {
+        Location location = new Location("Donson");
+        assertEquals("Donson", location.getName());
+    }
+    @Test
+    public void Location_instantiatesWithAnId() {
+        Location location = new Location("Donson");
+        location.save();
+        assertTrue(location.getId()>0);
+    }
+    @Test
+    public void save_savesCorrectly() {
+        Location location = new Location("Donson");
+        location.save();
+        assertTrue(Location.all().get(0).equals(location));
+    }
+    @Test
+    public void find_returnsLocationWIthSameID_secondLocation(){
+        Location location = new Location("Donson");
+        location.save();
+        Location locationTwo = new Location("Kalius");
+        locationTwo.save();
+        assertEquals(Location.find(locationTwo.getId()), locationTwo);
+    }
+    @Test
+    public void equals_returnsTrueIfLocationsAreSame(){
+        Location location = new Location("Donson");
+        Location locationTwo = new Location("Donson");
+        assertTrue(location.equals(locationTwo));
+    }
+    @Test
+    public void save_returnsTrueIfNamesAreTheSame(){
+        Location location = new Location("Donson");
+        location.save();
+        assertEquals(Location.all().get(0), location);
+    }
+    @Test
+    public void all_returnsAllInstancesOfLocations_true(){
+        Location locationOne = new Location("Donson");
+        locationOne.save();
+        Location locationTwo = new Location("Kalius");
+        locationTwo.save();
+        assertEquals(Location.all().get(0), locationOne);
+        assertEquals(Location.all().get(1), locationTwo);
+    }
+    @Test
+    public void save_assignsIdToObject() {
+        Location location = new Location("Donson");
+        location.save();
+        Location savedLocation = Location.all().get(0);
+        assertEquals(location.getId(), savedLocation.getId());
+    }
+    @Test
+    public void update_updateLocation_true(){
+        Location location = new Location("Donson");
+        location.save();
+        location.update("Simon", 90899);
+    }
+    @Test
+    public void delete_deletesLocation_true(){
+        Location location = new Location("Donson");
+        location.save();
+        int locationId = location.getId();
+        location.delete();
+        assertEquals(null, Location.find(locationId));
+    }
+}
