c09d52a0cf2f9ed3a2fb926c5edf061da93ef499
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-06-03 20:05:23 +0300

    Add update, equals, find, all and delete methods to sightings class.

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,4 +1,8 @@
+import org.sql2o.Connection;
+
+import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.List;
 
 public class Sighting {
     private int id;
@@ -27,6 +31,73 @@ public class Sighting {
 
     public int getLocationId() {
         return locationId;
+    }
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings (animalid, rangerid, locationid, date) VALUES(:animalid, :rangerid, " +
+                    ":locationid, now())";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("animalid", this.animalId)
+                    .addParameter("rangerid", this.rangerId)
+                    .addParameter("locationid", this.locationId)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Sighting> all(){
+
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM sightings")
+                    .executeAndFetch(Sighting.class);
+        }
+    }
+
+    public static Sighting find(int id){
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM sightings WHERE id=:id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sighting.class);
+        }
+    }
+
+    @Override
+    public boolean equals (Object otherSighting){
+        if (!(otherSighting instanceof Sighting)){
+            return false;
+        }else{
+            Sighting sighting =(Sighting) otherSighting;
+            return this.getId() == sighting.getId()&&
+                    this.getAnimalId()==sighting.getAnimalId() &&
+                    this.getLocationId()==sighting.getLocationId()&&
+                    this.getRangerId()==sighting.getRangerId();
+        }
+    }
+
+    public void update(int animalId, int locationId, int rangerId){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "UPDATE sightings SET animalId = :animalId, locationId = :locationId, rangerId = :rangerId, " +
+                    "date = now()" +
+                    " WHERE id" +
+                    " = " +
+                    ":id";
+            con.createQuery(sql)
+                    .addParameter("animalId", animalId)
+                    .addParameter("locationId", locationId)
+                    .addParameter("rangerId", rangerId)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    public void delete() {
+        try(Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM sightings WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
     }
 
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,2 +1,95 @@
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
 public class SightingTest {
+
+    @Before
+    public void setUp()  {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "verisence", "pass");
+    }
+
+    @After
+    public void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+            String sqlSighting = "DELETE FROM sightings *;";
+            con.createQuery(sqlSighting).executeUpdate();
+        }
+    }
+
+    @Test
+    public void Sighting_instantiatesCorrectly_true(){
+        Sighting sighting = new Sighting(1,2,3);
+        assertEquals(true, sighting instanceof Sighting);
+    }
+    @Test
+    public void Sighting_instantiatesWithAnimalId() {
+        Sighting sighting = new Sighting(1,2,3);
+        assertEquals(1, sighting.getAnimalId());
+    }
+    @Test
+    public void Sighting_instantiatesWithAnId() {
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        assertTrue(sighting.getId()>0);
+    }
+    @Test
+    public void save_savesCorrectly() {
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        assertTrue(Sighting.all().get(0).equals(sighting));
+    }
+    @Test
+    public void find_returnsSightingWIthSameID_secondSighting(){
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        Sighting sightingTwo = new Sighting(2,3,4);
+        sightingTwo.save();
+        assertEquals(Sighting.find(sightingTwo.getId()), sightingTwo);
+    }
+    @Test
+    public void equals_returnsTrueIfSightingsAreSame(){
+        Sighting sighting = new Sighting(1,2,3);
+        Sighting sightingTwo = new Sighting(1,2,3);
+        assertTrue(sighting.equals(sightingTwo));
+    }
+    @Test
+    public void save_returnsTrueIfNamesAreTheSame(){
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        assertEquals(Sighting.all().get(0), sighting);
+    }
+    @Test
+    public void all_returnsAllInstancesOfSightings_true(){
+        Sighting sightingOne = new Sighting(1,2,3);
+        sightingOne.save();
+        Sighting sightingTwo = new Sighting(2,3,4);
+        sightingTwo.save();
+        assertEquals(Sighting.all().get(0), sightingOne);
+        assertEquals(Sighting.all().get(1), sightingTwo);
+    }
+    @Test
+    public void save_assignsIdToObject() {
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(sighting.getId(), savedSighting.getId());
+    }
+    @Test
+    public void update_updateSighting_true(){
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        sighting.update(2,3,4);
+    }
+    @Test
+    public void delete_deletesSighting_true(){
+        Sighting sighting = new Sighting(1,2,3);
+        sighting.save();
+        int sightingId = sighting.getId();
+        sighting.delete();
+        assertEquals(null, Sighting.find(sightingId));
+    }
 }
