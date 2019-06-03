b2564d607ded3a7d9d57ce560b56096e81546921
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-06-03 15:56:54 +0300

    Add Methods to Animal class such as save(), update(), overridden equals() and delete() and their tests

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,8 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Animal {
     private int id;
     private String name;
@@ -19,5 +24,67 @@ public class Animal {
 
     public String getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+        Animal animal = (Animal) o;
+        return getId() == animal.getId() &&
+                getName().equals(animal.getName()) &&
+                getType().equals(animal.getType());
+    }
+
+
+    public void save() {
+        String sql = "INSERT INTO animals (name, type) VALUES (:name, :type)";
+        try(Connection con = DB.sql2o.open()) {
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", name)
+                    .addParameter("type", type)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    
+    public static List<Animal> all(){
+
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT id, name, type FROM animals";
+            return con.createQuery(sql).executeAndFetch(Animal.class);
+        }
+    }
+
+    public static Animal find(int id){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM animals where id=:id";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(Animal.class);
+        }
+    }
+
+    public void update(String name) {
+        String sql = "UPDATE animals SET name = :name WHERE id = :id";
+
+        try(Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeUpdate();
+        }
+    }
+
+    public void delete() {
+        try(Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM animals WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
     }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,3 +1,100 @@
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
 public class AnimalTest {
 
+    @Before
+    public void setUp()  {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "verisence", "pass");
+    }
+
+    @After
+    public void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+            String sqlAnimal = "DELETE FROM animals *;";
+            con.createQuery(sqlAnimal).executeUpdate();
+        }
+    }
+
+    @Test
+    public void Animal_instantiatesCorrectly_true(){
+        Animal animal = new Animal("Chui");
+        assertEquals(true, animal instanceof  Animal);
+    }
+    @Test
+    public void Animal_instantiatesWithName_String() {
+        Animal animal = new Animal("Chui");
+        assertEquals("Chui", animal.getName());
+    }
+    @Test
+    public void Animal_instantiatesWithType_String() {
+        Animal animal = new Animal("Chui");
+        assertEquals("animal", animal.getType());
+    }
+    @Test
+    public void Animal_instantiatesWithAnId() {
+        Animal animal = new Animal("Chui");
+        animal.save();
+        assertTrue(animal.getId()>0);
+    }
+    @Test
+    public void save_savesCorrectly() {
+        Animal animal = new Animal("Chui");
+        animal.save();
+        assertTrue(Animal.all().get(0).equals(animal));
+    }
+    @Test
+    public void find_returnsAnimalWIthSameID_secondAnimal(){
+        Animal animalOne = new  Animal("Chui");
+        animalOne.save();
+        Animal animalTwo = new Animal("Impalla");
+        animalTwo.save();
+        assertEquals(Animal.find(animalTwo.getId()), animalTwo);
+    }
+    @Test
+    public void equals_returnsTrueIfAnimalsAreSame(){
+        Animal animalOne = new  Animal("Chui");
+        Animal animalTwo = new Animal("Chui");
+        assertTrue(animalOne.equals(animalTwo));
+    }
+    @Test
+    public void save_returnsTrueIfNamesAreTheSame(){
+        Animal animal = new Animal("Chui");
+        animal.save();
+        assertEquals(Animal.all().get(0), animal);
+    }
+    @Test
+    public void all_returnsAllInstancesOfAnimals_true(){
+        Animal animalOne = new  Animal("Chui");
+        animalOne.save();
+        Animal animalTwo = new Animal("Impala");
+        animalTwo.save();
+        assertEquals(Animal.all().get(0), animalOne);
+        assertEquals(Animal.all().get(1), animalTwo);
+    }
+    @Test
+    public void save_assignsIdToObject() {
+        Animal animal = new Animal("Chui");
+        animal.save();
+        Animal savedAnimal = Animal.all().get(0);
+        assertEquals(animal.getId(), savedAnimal.getId());
+    }
+    @Test
+    public void update_updateAnimal_true(){
+        Animal animal = new Animal("Chui");
+        animal.save();
+        animal.update("Impala");
+    }
+    @Test
+    public void delete_deletesAnimal_true(){
+        Animal animal = new Animal("Chui");
+        animal.save();
+        int animalId = animal.getId();
+        animal.delete();
+        assertEquals(null, Animal.find(animalId));
+    }
 }
