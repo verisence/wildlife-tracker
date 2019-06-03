6e7265399087ab3aa1a610b70c3073d7eb2bde8e
Kipkemboi Phil <Kipkemboiphil@gmail.com>
2019-06-03 18:51:34 +0300

    Add methods to ranger class such as update, delete find and all

--- src/main/java/Ranger.java
+++ src/main/java/Ranger.java
@@ -1,3 +1,6 @@
+import org.sql2o.Connection;
+
+import java.util.List;
 import java.util.Objects;
 
 public class Ranger {
@@ -23,17 +26,60 @@ public class Ranger {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Ranger)) return false;
-        Ranger ranger = (Ranger) o;
-        return getId() == ranger.getId() &&
-                getBadgeNumber() == ranger.getBadgeNumber() &&
-                getName().equals(ranger.getName());
+    public boolean equals(Object otherObject) {
+        if(!(otherObject instanceof Ranger))
+            return false;
+        else {
+            Ranger comparisonObj = (Ranger) otherObject;
+            return this.id == comparisonObj.getId() && this.name.equals(comparisonObj.getName()) && this.badgeNumber==
+                    comparisonObj.getBadgeNumber();
+        }
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getBadgeNumber());
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO rangers (name, badgenumber) VALUES(:name, :badgenumber)";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("name", this.name)
+                    .addParameter("badgenumber", this.badgeNumber)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Ranger> all(){
+
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM rangers")
+                    .executeAndFetch(Ranger.class);
+        }
+    }
+
+    public static Ranger find(int id){
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery("SELECT * FROM rangers WHERE id=:id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Ranger.class);
+        }
+    }
+
+    public void update(String name, int badgeNumber){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "UPDATE rangers SET name = :name, badgenumber = :badgenumber WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("badgenumber", badgeNumber)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    public void delete() {
+        try(Connection con = DB.sql2o.open()){
+            String sql = "DELETE FROM rangers WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
     }
 }
--- src/test/java/RangerTest.java
+++ src/test/java/RangerTest.java
@@ -1,2 +1,95 @@
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
 public class RangerTest {
+
+    @Before
+    public void setUp()  {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "verisence", "pass");
+    }
+
+    @After
+    public void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+            String sqlRanger = "DELETE FROM rangers *;";
+            con.createQuery(sqlRanger).executeUpdate();
+        }
+    }
+
+    @Test
+    public void Ranger_instantiatesCorrectly_true(){
+        Ranger ranger = new Ranger("Donson", 89898);
+        assertEquals(true, ranger instanceof Ranger);
+    }
+    @Test
+    public void Ranger_instantiatesWithName_String() {
+        Ranger ranger = new Ranger("Donson", 89898);
+        assertEquals("Donson", ranger.getName());
+    }
+    @Test
+    public void Ranger_instantiatesWithAnId() {
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        assertTrue(ranger.getId()>0);
+    }
+    @Test
+    public void save_savesCorrectly() {
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        assertTrue(Ranger.all().get(0).equals(ranger));
+    }
+    @Test
+    public void find_returnsRangerWIthSameID_secondRanger(){
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        Ranger rangerTwo = new Ranger("Kalius", 89798);
+        rangerTwo.save();
+        assertEquals(Ranger.find(rangerTwo.getId()), rangerTwo);
+    }
+    @Test
+    public void equals_returnsTrueIfRangersAreSame(){
+        Ranger ranger = new Ranger("Donson", 89898);
+        Ranger rangerTwo = new Ranger("Donson", 89898);
+        assertTrue(ranger.equals(rangerTwo));
+    }
+    @Test
+    public void save_returnsTrueIfNamesAreTheSame(){
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        assertEquals(Ranger.all().get(0), ranger);
+    }
+    @Test
+    public void all_returnsAllInstancesOfRangers_true(){
+        Ranger rangerOne = new Ranger("Donson", 89898);
+        rangerOne.save();
+        Ranger rangerTwo = new Ranger("Kalius", 89798);
+        rangerTwo.save();
+        assertEquals(Ranger.all().get(0), rangerOne);
+        assertEquals(Ranger.all().get(1), rangerTwo);
+    }
+    @Test
+    public void save_assignsIdToObject() {
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        Ranger savedRanger = Ranger.all().get(0);
+        assertEquals(ranger.getId(), savedRanger.getId());
+    }
+    @Test
+    public void update_updateRanger_true(){
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        ranger.update("Simon", 90899);
+    }
+    @Test
+    public void delete_deletesRanger_true(){
+        Ranger ranger = new Ranger("Donson", 89898);
+        ranger.save();
+        int rangerId = ranger.getId();
+        ranger.delete();
+        assertEquals(null, Ranger.find(rangerId));
+    }
 }
